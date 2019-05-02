c088b6841ab1cd52726f05b71ec5bfa1397012bf
Filipe <filipe_1018@hotmail.com>
2019-05-02 12:00:54 +0200

    d

---
+++ Filipe/TCP-IP/Client/frmClient.cs
@@ -0,0 +1,104 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Client
+{
+    public partial class ClientApp : Form
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public ClientApp()
+        {
+            InitializeComponent();
+            btnSend.Enabled = false;
+        }
+
+        public string Nickname
+        {
+            get { return txtNickName.Text; }
+        }
+
+        SimpleTcpClient client; // Client
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {                
+                client.Connect(txtHost.Text, Convert.ToInt32(txtPort.Text));
+                btnSend.Enabled = true; // active le bouton
+                btnConnect.Enabled = false; // desactive le bouton
+            }
+            catch (Exception ex)
+            {
+               txtStatus.Text = ex.Message;
+               txtStatus.Text = "Il n'y a aucun serveur de disponible";
+            }           
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new SimpleTcpClient(); 
+            client.StringEncoder = Encoding.UTF8;
+            client.DataReceived += Client_DataReceived;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Client_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            txtStatus.Invoke((MethodInvoker)delegate ()
+            {
+                string nickName = txtNickName.Text;
+                string myMessage = e.MessageString.Substring(0, e.MessageString.Length);
+                txtStatus.Text += myMessage + Environment.NewLine;
+                if(myMessage == "Server has been shutdown")
+                {
+                    btnConnect.Enabled = true;
+                }
+            });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            if (txtMessage.Text.Trim() == "")
+            {
+
+            }
+            else
+            {
+                string name = (sender as Control).Name;
+                name = txtNickName.Text;
+                client.WriteLineAndGetReply(name + " a écrit: " + txtMessage.Text, TimeSpan.FromSeconds(0));
+                txtMessage.Clear();
+            }           
+        }
+    }
+}
---
+++ Filipe/TCP-IP/Serveur/frmServer.cs
@@ -0,0 +1,74 @@
+﻿using Client;
+using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Serveur
+{
+    public partial class Server : Form
+    {
+
+        ClientApp _client = new ClientApp();
+
+        public Server()
+        {
+            InitializeComponent();          
+        }
+
+        
+        public SimpleTcpServer server; // Tcp Serveur
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            server = new SimpleTcpServer(); // instanciation du serveur
+            server.Delimiter = 0x13;
+            server.StringEncoder = Encoding.UTF8;
+            server.DataReceived += Server_DataReceived;
+            server.GetListeningIPs();
+        }
+
+        private void Server_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            
+            txtStatus.Invoke((MethodInvoker)delegate ()
+            {
+                string myMessage = e.MessageString.Substring(0, e.MessageString.Length - 1); // récupère la string sauf le dernier caractère
+                //txtStatus.Text += myMessage + Environment.NewLine; // display du message               
+                txtStatus.Text +=  Environment.NewLine;
+                server.Broadcast(myMessage);
+            });
+        }
+
+        private void BtnStart_Click(object sender, EventArgs e)
+        {
+            txtStatus.Text += "Server starting... " + Environment.NewLine;
+            System.Net.IPAddress ip = System.Net.IPAddress.Parse(txtHost.Text); // récupère l'ip
+            Debug.WriteLine(ip);
+            server.Start(ip, Convert.ToInt32(txtPort.Text)); // le serveur se lance avec l'ip et le port
+            txtStatus.Text += "Connected to the server." + Environment.NewLine;
+            btnStart.Enabled = false;
+        }
+
+        public void BtnStop_Click(object sender, EventArgs e)
+        {
+            
+            if (server.IsStarted)
+            {
+                server.Broadcast("Server has been shutdown");
+                server.Stop(); // arrete le serveur si il est lancé
+                txtStatus.Text += "Server Stopped" + Environment.NewLine;               
+                btnStart.Enabled = true;
+            }
+
+        }
+    }
+}
